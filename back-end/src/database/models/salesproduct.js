bffb6fb6c401817338fcd259e8170c6f7a39dff9
Aleilton Chavenco <aleilton@gmail.com>
2022-02-22 17:15:17 -0300

    feat: create models SalesProducts

---
+++ back-end/src/database/models/salesproduct.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, _DataTypes) => {
+  const salesProduct = sequelize.define('SalesProduct',
+    {
+      quantity: DataTypes.INTEGER,
+    }, { timestamps: false });
+
+    salesProduct.associate = (models) => {
+    models.Product.belongsToMany(models.Sale, {
+      as: 'sales',
+      through: salesProduct,
+      foreignKey: 'product_id',
+      otherKey: 'sale_id',
+    });
+    models.Sale.belongsToMany(models.Product, {
+      as: 'products',
+      through: salesProduct,
+      foreignKey: 'sale_id',
+      otherKey: 'product_id',
+    });
+  };
+
+  return salesProduct;
+};
