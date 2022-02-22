553154deae277a52da5a5eca45c22d06f93d9fa7
Aleilton Chavenco <aleilton@gmail.com>
2022-02-22 18:45:23 -0300

    feat: create errorHandler define

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = require('../routes');
+const errorHandler = require('../middlewares/errorHandler');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(router);
+app.use(errorHandler);
 
 module.exports = app;
--- back-end/src/middlewares/errorHandler.js
+++ back-end/src/middlewares/errorHandler.js
@@ -1,11 +1,10 @@
 module.exports = (error, _req, res, _next) => {
-  if (error.message) {
+  const { status, message } = error;
+  if (message) {
     console.log(`errorHandler: ${error}`);
-    console.log(`errorHandler: ${error.status}`);
-    console.log(`errorHandler: ${error.message}`);
-    return res.status(error.status).json({
-      message: error.message,
-    });
+    console.log(`errorHandler: ${status}`);
+    console.log(`errorHandler: ${message}`);
+    return res.status(status).json({ message });
   }
   return res.status(500).json({ message: 'Internal Server Error' });
 };
