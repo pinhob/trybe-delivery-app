fc1c80dd19cece7fb1d4165f4f499e50b3f45d0e
Aleilton Chavenco <aleilton@gmail.com>
2022-02-22 16:17:56 -0300

    feat: create models User

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING,
+  }, {
+    timestamps: false,
+  });
+
+  user.associate = (models) => {
+    user.hasMany(models.Sale, { foreignKey: 'user_id', as: 'sale' });
+    user.hasMany(models.Sale, { foreignKey: 'seller_id', as: 'sale' });
+  };
+
+  return user;
+};
