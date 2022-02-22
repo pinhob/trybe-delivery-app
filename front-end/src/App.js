17a2cb575b3be341ebd27f981106a351d26901c7
Gustavo-Jezini <gustavojezini@gmail.com>
2022-02-22 17:40:46 -0300

    feat: css para navBars em andamento

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Cadastro from './components/Comum/Cadastro/Cadastro';
 import Login from './components/Comum/Login/Login';
 import Comum from './components/Comum/Comum/Comum';
+import NavBarCliente from './components/NavsBar/ClienteNavBar';
 
 function App() {
   return (
     <Switch>
+      <Route path="/testNavBar" component={ NavBarCliente } />
       <Route exact path="/" component={ Comum } />
       <Route path="/login" component={ Login } />
       <Route path="/cadastro" component={ Cadastro } />
