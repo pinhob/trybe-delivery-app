68a11583a266dc2fec1aedd90a4e38ea0d6c750e
Gustavo-Jezini <gustavojezini@gmail.com>
2022-02-22 12:05:22 -0300

    style: Corrige erros de lint

--- front-end/src/components/Comum/Comum/Comum.js
+++ front-end/src/components/Comum/Comum/Comum.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory } from 'react-router-dom';
 
 const Comum = () => {
   const history = useHistory();
 
   useEffect(() => {
     history.push('/login');
-  }, [])
+  }, [history]);
 
   return (
     <>
-    Loading
+      Loading
     </>
-  )
-}
+  );
+};
 
 export default Comum;
