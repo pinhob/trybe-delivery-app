bb9d52b016437af591e360833079d761ad1c1f37
Aleilton Chavenco <aleilton@gmail.com>
2022-02-22 19:04:11 -0300

    feat: initial setup

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { User } = require('../database/models');
+
 const router = require('../routes');
 const errorHandler = require('../middlewares/errorHandler');
 
@@ -10,6 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.get('/', async (_req, res) => {
+  const result = await User.findAll();
+  res.status(200).json(result);
+});
 
 app.use(router);
 app.use(errorHandler);
