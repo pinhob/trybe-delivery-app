7af34d447e9a6c36f9cc08a9bab0d0fcb9f6304a
Aleilton Chavenco <aleilton@gmail.com>
2022-02-23 10:42:56 -0300

    feat: getAll User Router

--- back-end/src/routes/user.js
+++ back-end/src/routes/user.js
@@ -5,5 +5,6 @@ const userRouter = express.Router();
 const userControllers = require('../controllers/user');
 
 userRouter.post('/', userControllers.create);
+userRouter.get('/', userControllers.getAll);
 
 module.exports = userRouter;
