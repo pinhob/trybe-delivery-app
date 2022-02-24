a0eb34df964d8f6dd16cfcddb01a1c41d74a2fa3
Aleilton Chavenco <aleilton@gmail.com>
2022-02-24 00:50:43 -0300

    feat: getAll Sale router

--- back-end/src/routes/sale.js
+++ back-end/src/routes/sale.js
@@ -7,5 +7,6 @@ const auth = require('../middlewares/auth');
 const saleControllers = require('../controllers/sale');
 
 saleRouter.post('/', auth, saleControllers.create);
+saleRouter.get('/', auth, saleControllers.getAll);
 
 module.exports = saleRouter;
