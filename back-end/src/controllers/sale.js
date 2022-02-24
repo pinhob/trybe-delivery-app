44deb5cff1ab6d38c1b1795ac510811c195b3dbe
Aleilton Chavenco <aleilton@gmail.com>
2022-02-24 17:22:24 -0300

    feat: getAll Sales include seller return

--- back-end/src/controllers/sale.js
+++ back-end/src/controllers/sale.js
@@ -4,8 +4,10 @@ const create = async (req, res, next) => {
   try {
     const { body, loggedUser } = req;
     const { id: userId } = loggedUser;
-    const { totalPrice, deliveryAddress, deliveryNumber, status = 'Pendente', products } = body;
-    const sale = { userId, totalPrice, deliveryAddress, deliveryNumber, status, products };
+    const {
+      sellerId, totalPrice, deliveryAddress, deliveryNumber, status = 'Pendente', products } = body;
+    const sale = {
+      userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, products };
     const result = await SaleServices.create(sale);
     return res.status(201).json(result);
   } catch (error) {
--- back-end/src/services/sale.js
+++ back-end/src/services/sale.js
@@ -7,6 +7,7 @@ const ERROR = require('../utils/messagesError');
 
 const saleSchema = Joi.object({
   userId: Joi.number().required(),
+  sellerId: Joi.number().required(),
   totalPrice: Joi.number().precision(2).required(),
   deliveryAddress: Joi.string().required(),
   deliveryNumber: Joi.string().required(),
@@ -25,16 +26,17 @@ const validateProducts = async (products) => products.map(async (product) => {
 
 const getQueryFilters = (seller, status, user) => {
   const query = {};
-  if (seller !== '') query.selerId = seller;
+  if (seller !== '') query.sellerId = seller;
+  if (seller === 'null') query.sellerId = null;
   if (status !== '') query.status = status;
   if (user !== '') query.userId = user;
   return query;
 };
 
 const create = async (sale) => {
-  const { userId, totalPrice, deliveryAddress, deliveryNumber, status, products } = sale;
+  const { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, products } = sale;
   const { error } = saleSchema
-    .validate({ userId, totalPrice, deliveryAddress, deliveryNumber, status });
+    .validate({ userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status });
   if (error) {
     const err = new Error(error.details.map((errorObj) => errorObj.message).toString());
     err.code = 'invalid_data';
@@ -46,7 +48,7 @@ const create = async (sale) => {
   await Promise.all(productsPromise);
 
   const result = await Sale
-    .create({ userId, totalPrice, deliveryAddress, deliveryNumber, status });
+    .create({ userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status });
 
   await products.map(async (product) => {
     const { productId, quantity } = product;
@@ -56,22 +58,23 @@ const create = async (sale) => {
   return result;
 };
 
+const includeObjectSale = [
+  { model: User, as: 'user', attributes: { exclude: ['password'] } },
+  { model: User, as: 'seller', attributes: { exclude: ['password'] } },
+  { model: Product, as: 'products', through: { attributes: ['quantity'] } },
+];
+
 const getAll = async (seller, status, user) => {
   let result = [];
+
   if (seller === '' && status === '' && user === '') {
     result = await Sale.findAll({
-      include: [
-        { model: User, as: 'user', attributes: { exclude: ['password'] } },
-        { model: Product, as: 'products', through: { attributes: [] } },
-      ],
+      include: includeObjectSale,
     });
   } else {
     const query = getQueryFilters(seller, status, user);
     result = await Sale.findAll({ where: query,
-      include: [
-        { model: User, as: 'user', attributes: { exclude: ['password'] } },
-        { model: Product, as: 'products', through: { attributes: [] } },
-      ],
+      include: includeObjectSale,
     });
   }
   return result;
@@ -79,10 +82,7 @@ const getAll = async (seller, status, user) => {
 
 const getById = async (id) => {
   const result = await Sale.findByPk(id, {
-    include: [
-      { model: User, as: 'user', attributes: { exclude: ['password'] } },
-      { model: Product, as: 'products', through: { attributes: [] } },
-    ],
+    include: includeObjectSale,
   });
 
   if (!result) throw (errorObject(ERROR.MESSAGE_SALE_NOT_EXISTS, ERROR.STATUS_BAD_REQUEST));
