795eff9941a7e0bafe30f6752813acb88efa7f36
Aleilton Chavenco <aleilton@gmail.com>
2022-02-24 19:20:23 -0300

    feat: update Sales router

--- back-end/src/routes/sale.js
+++ back-end/src/routes/sale.js
@@ -9,5 +9,6 @@ const saleControllers = require('../controllers/sale');
 saleRouter.post('/', auth, saleControllers.create);
 saleRouter.get('/', auth, saleControllers.getAll);
 saleRouter.get('/:id', auth, saleControllers.getById);
+saleRouter.put('/:id', auth, saleControllers.update);
 
 module.exports = saleRouter;
