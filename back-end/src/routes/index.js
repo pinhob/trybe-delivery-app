2d2dc3c5959a467891dc60ee56b0183bfb98a91c
Aleilton Chavenco <aleilton@gmail.com>
2022-02-23 21:20:24 -0300

    feat: Login Router

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./user');
+const loginRouter = require('./login');
 
 router.use('/users', userRouter);
+router.use('/login', loginRouter);
 
 module.exports = router;
