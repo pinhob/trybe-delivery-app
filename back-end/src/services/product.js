cb70fe6e606f2a91fe92d04cf88a7e2df102ea8d
Aleilton Chavenco <aleilton@gmail.com>
2022-02-23 22:02:27 -0300

    feat: getById Product Services

--- back-end/src/services/product.js
+++ back-end/src/services/product.js
@@ -1,10 +1,20 @@
 const { Product } = require('../database/models');
 
+const { errorObject } = require('../utils/errorObject');
+const ERROR = require('../utils/messagesError');
+
 const getAll = async () => {
   const result = await Product.findAll();
   return result;
 };
 
+const getById = async (id) => {
+  const result = await Product.findByPk(id);
+  if (!result) throw (errorObject(ERROR.MESSAGE_PROD_NOT_EXISTS, ERROR.STATUS_NOT_FOUND));
+  return result;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
