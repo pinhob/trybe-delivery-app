83feff2046382efe885c7702ae290ce051f3b074
Bruno Pinho <brunopinho@outlook.com>
2022-03-04 16:16:30 -0300

    feat: instala crypto-js. reverte componente Login

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -8,9 +8,9 @@ export const fetchAllProducts = (token) => axios.get(`${URL}/products`,
 export const createUser = (user) => axios.post(`${URL}/users`, user);
 
 // Catch feito com base em: https://axios-http.com/docs/handling_errors
-export const loginUser = (name, password) => axios.post(`${URL}/login`, { name,
-  password })
-  .catch((error) => error.toJSON());
+export const loginUser = (email, password) => axios.post(`${URL}/login`, { email,
+  password });
+  // .catch((error) => error.toJSON());
 
 export const getSellers = (token) => axios.get(`${URL}/users/sellers`, { headers:
   { authorization: `${token}` } });
